22fa3a1dd4669ebed0a723aa71cfca5c3c18e76b
amr abdulsalam <amrsalam@hotmail.com>
2020-03-20 17:24:02 -0700

    starting project

---
+++ src/com/company/Book.java
@@ -0,0 +1,80 @@
+package com.company;
+
+import java.time.chrono.IsoChronology;
+import java.util.ArrayList;
+
+public class Book {
+
+    private String ISBN ;
+    private String Title;
+    private String Author;
+    private int Edition ;
+    private int puplishedYear ;
+    private boolean Borrowed;
+    public int counter = 0 ;
+    //public int index;
+
+
+    //There should be an arrayList for each data type with a counter
+    public ArrayList<String> arrISBN = new ArrayList<String>();
+    private ArrayList<String> arrTitle = new ArrayList<String>();
+    private ArrayList<String> arrAuthor = new ArrayList<String>();
+    private ArrayList<Integer> arrEdition = new ArrayList<Integer>();
+    private ArrayList<Integer> arrPuplish = new ArrayList<Integer>();
+    private ArrayList<Boolean> arrBorrowed = new ArrayList<Boolean>();
+
+
+    public int sizeR(){
+        return arrISBN.size();
+    }
+
+
+    // Set method for all primitve types :
+    public void setISBN(String ISBN ){
+
+        arrISBN.add(ISBN);
+    }
+    public void setTitle(String Title ,int index){
+        arrTitle.add(index ,Title);
+    }
+    public void setAuthor(String Aurthor ,int index){
+        arrAuthor.add(index ,Aurthor);
+    }
+    public void setEdition(int Edition ,int index){
+        arrEdition.add(index ,Edition);
+    }
+    public void setPuplishedYear(int puplishedYear ,int index){
+        arrPuplish.add(index ,puplishedYear);
+    }
+    public void setBorrowed(boolean Borrowed ,int index){
+        arrBorrowed.add(index ,Borrowed);
+    }
+
+    //---------------
+    //Get methods using arrayLists
+
+    public String getISBN(int i){
+
+        return arrISBN.get(i);
+    }
+
+    public String getTitle(int i){
+        return arrTitle.get(i);
+    }
+
+    public String getAuthor(int i){
+        return arrAuthor.get(i);
+    }
+
+    public int getPuplishedYear(int i){
+        return arrPuplish.get(i);
+    }
+
+    public int getEdition(int i){
+        return arrEdition.get(i);
+    }
+
+    public boolean getBorrowed(int i){
+        return arrBorrowed.get(i);
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,159 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String [] s = new String[5];//Dose not do anything
+
+        Scanner input = new Scanner(System.in);
+        int num1 , index , num2;
+        String output = "";
+        System.out.println("Welcome to my program please start using the given COMANDS : ");
+        Book book = new Book();
+        for(; ; ) {
+
+
+            System.out.print("> "); // for printing like cmd
+
+            String comands = input.nextLine(); // for reading the Comands
+
+            String[] tokens = comands.split(" ");
+            tokens[0] = tokens[0].toLowerCase(); // to make the first command all with Small letters
+
+            //Starting switch case to know which command to do.
+
+            switch (tokens[0]) {
+                case "add":
+                    //System.out.println("Its add command");
+                    if (tokens.length != 2)
+                        System.out.println("Invalid inputs");
+                    else {
+
+                        //book.setISBN(tokens[1]);
+                        book.setISBN(tokens[1]);
+                        //System.out.println(tokens.length);
+                        System.out.println("You have added a new book congratulations :)");
+                        book.counter++;
+                        index = book.arrISBN.indexOf(tokens[1]);
+                        book.setBorrowed(false , index);
+                    }
+
+                    break;
+
+                case "set":
+
+                    for(int i = 3 ; i < tokens.length ;i++){
+                        output += tokens[i] + " " ;
+                    }
+                        index = book.arrISBN.indexOf(tokens[1]);
+
+                        switch (tokens[2]){
+                            case "title":
+                                book.setTitle(output , index );
+                            break;
+
+                            case "author" :
+                                book.setAuthor(output , index);
+                            break;
+
+                            case "published_year" :
+                                num1 = Integer.parseInt(tokens[3]);
+                                book.setPuplishedYear(num1 , index);
+                            break;
+
+                            case "edition" :
+                                num1 = Integer.parseInt(tokens[3]);
+                                book.setEdition(num1, index);
+                            break;
+
+                    }
+                    output = "";
+                    break;
+
+                case "delete":
+                    System.out.println("Its delete command");
+                    break;
+
+                case "get":
+                    //System.out.println("Its get command");
+
+                    index = book.arrISBN.indexOf(tokens[1]);
+
+                    switch (tokens[2]){
+
+                        case "title" :
+                            System.out.println(book.getTitle(index));
+                        break;
+
+                        case "author" :
+                            System.out.println(book.getAuthor(index));
+                        break;
+
+                        case "published_year" :
+                            //num1 = Integer.parseInt(tokens[2]);
+                            System.out.println(book.getPuplishedYear(index));
+                         break;
+
+                        case "edition" :
+                            System.out.println(book.getEdition(index));
+                        break;
+                        }
+                    break;
+
+                case "rent":
+                    //System.out.println("Its rent command");
+                    index = book.arrISBN.indexOf(tokens[1]);
+                    if(!book.getBorrowed(index)){
+                        System.out.println("You borrow a book please come again");
+                        book.setBorrowed(true , index);
+                    }
+                    else{
+                        System.out.println("Sorry the book is already rented");
+                    }
+                    break;
+
+                case "return":
+                    //System.out.println("Its return command");
+                    index = book.arrISBN.indexOf(tokens[1]);
+                    if(!book.getBorrowed(index))
+                        System.out.println("This book is already available");
+                    else{
+                        System.out.println("Than you for returning the book ");
+                        book.setBorrowed(false,index);
+                    }
+                    break;
+
+                case "search":
+                    System.out.println("Its search command");
+                    break;
+
+                case "print":
+                    //System.out.println("Its print command");
+
+                    output = "ISBN\t\t" +"Title\t\t\t\t\t" + "Edition\t\t\t\t" + "Author\t\t\t" + "Year\t\t\t" + "Available";
+                    System.out.println(output);
+
+                    for(int i = 0 ; i < book.counter ; i++){
+                        output = book.getISBN(i) + "\t\t\t" + book.getTitle(i) +"\t\t\t" +
+                                book.getEdition(i) +"th"+"\t\t\t" + book.getAuthor(i) + "\t  " + book.getPuplishedYear(i)
+                        +"\t\t\t" +(book.getBorrowed(i) ? "Not Available" : "Available");
+
+                        System.out.println(output);
+                    }
+
+                    break;
+
+                case "quit":
+                    System.out.println("Thank you for using our program ");
+                    System.exit(0);
+                    break;
+
+                default:
+                    System.out.println("Sorry you entered a wrong COMAND !!");
+            }
+
+
+        }
+    }
+}
